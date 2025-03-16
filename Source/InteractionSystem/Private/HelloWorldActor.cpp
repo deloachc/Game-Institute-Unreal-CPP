899dc32e9160de274f5f2f7da7d697d2e39889f9
deloachc <cdeloachwork@gmail.com>
2025-03-16 14:16:23 -0700

    12 Else If Statements

--- Source/InteractionSystem/Private/HelloWorldActor.cpp
+++ Source/InteractionSystem/Private/HelloWorldActor.cpp
@@ -16,21 +16,9 @@ void AHelloWorldActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	if (bClassBool)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("ClassBool is true"));
-	}
-	
-	if (!bClassBool)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("ClassBool is false"));
-	}
-
-	int32 LocalInteger = 4;
-
-	float LocalFloat = 1.2f;
-
-	UE_LOG(LogTemp, Warning, TEXT("ClassInteger: %i, LocalFloat: %f, LocalInteger: %i"), ClassInteger, LocalFloat, LocalInteger);
+	CompareTwoFloats(1.2f, 2.5f);
+	CompareTwoFloats(3.5f, 2.2f);
+	CompareTwoFloats(1.1f, 1.1f);
 
 }
 
@@ -49,6 +37,22 @@ void AHelloWorldActor::PrintHelloWorldToLog()
 int32 AHelloWorldActor::AddTwoIntegers(int32 A, int32 B)
 {
 	return A + B;
+}
+
+void AHelloWorldActor::CompareTwoFloats(float A, float B)
+{
+	if (A > B)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%f is greater than %f"), A, B);
+	}
+	else if (A < B)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%f is less than %f"), A, B);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%f is equal to %f"), A, B);
+	}
 }
 
 void AHelloWorldActor::CompareClassIntegerToInput(int32 Input)
