638518b1f46f25cbed6f38625f83e4fd37f8eae7
deloachc <cdeloachwork@gmail.com>
2025-04-06 14:12:38 -0700

    42 UMG in C++

---
+++ Source/InteractionSystem/Private/Player/InteractionPlayerController.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/InteractionPlayerController.h"
+
+#include "Blueprint/UserWidget.h"
+
+void AInteractionPlayerController::ShowCanvas()
+{
+	if (!CanvasWidget)
+	{
+		CanvasWidget = CreateWidget(this, CanvasWidgetClass);
+	}
+	
+	CanvasWidget->AddToViewport();
+}
+
+void AInteractionPlayerController::HideCanvas()
+{
+	if (CanvasWidget)
+	{
+		CanvasWidget->RemoveFromParent();
+	}
+}
+
+void AInteractionPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ShowCanvas();
+}
---
+++ Source/InteractionSystem/Public/Player/InteractionPlayerController.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "InteractionPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTERACTIONSYSTEM_API AInteractionPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+
+	void ShowCanvas();
+
+	void HideCanvas();
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UUserWidget* CanvasWidget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UUserWidget> CanvasWidgetClass;
+};
